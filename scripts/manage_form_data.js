4d0ba8e09219293b367621ab2bdd9c19a52a37d1
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2023-12-26 15:27:51 +0530

    Scripting continous in manage_form_data.js, by adding conditional statements inside validateRegisterFormData() + created displayError(), displaySuccess() function which displays error msg bottom of input tags and replace is-invalid class to is-valid in input tag. validateEmail() and validatePassword() validates and checks email and password pattern, lastly finalizeRegisterSuccess() method checks is all input tags are valid or all data's are good if good then call showAlert() method to pop-up success alert using sweetalert

--- scripts/manage_form_data.js
+++ scripts/manage_form_data.js
@@ -9,23 +9,23 @@ DisplayRegisterForm = () => {
                 <div class="formcontrol mb-3">
                     <label for="InputUsernameforRegister" class="form-label" onclick="HapticOn()">Username</label>
                     <input type="text" class="form-control" id="InputUsernameforRegister" aria-describedby="RegisterUsernameHelp" onchange="HapticOn()">
-                    <div id="RegisterUsernameHelp" class="form-text"></div>
+                    <span id="RegisterUsernameHelp" class="form-text"></span>
                 </div>
                 <div class="formcontrol mb-3">
                     <label for="InputEmailforRegister" class="form-label" onclick="HapticOn()">Email address</label>
                     <input type="email" class="form-control" id="InputEmailforRegister" aria-describedby="RegisterEmailHelp" onchange="HapticOn()">
-                    <div id="RegisterEmailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <span id="RegisterEmailHelp" class="form-text">We'll never share your email with anyone else.</span>
                 </div>
                 
                 <div class="formcontrol mb-3">
                     <label for="InputPasswordforRegister" class="form-label" onclick="HapticOn()">Password</label>
                     <input type="password" class="form-control" id="InputPasswordforRegister" aria-describedby="RegisterPasswordHelp" onchange="HapticOn()">
-                    <div id="RegisterPasswordHelp" class="form-text text-danger"></div>
+                    <span id="RegisterPasswordHelp" class="form-text text-danger"></span>
                 </div>
                 <div class="formcontrol mb-3">
                     <label for="InputConfirmPasswordforRegister" class="form-label" onclick="HapticOn()">Confirm Password</label>
                     <input type="password" class="form-control" id="InputConfirmPasswordforRegister" aria-describedby="RegisterCPasswordHelp" onchange="HapticOn()">
-                    <div id="RegisterCPasswordHelp" class="form-text text-danger"></div>
+                    <span id="RegisterCPasswordHelp" class="form-text text-danger"></span>
                 </div>
                 
                 <button type="submit" class="btn btn-dark" onclick="HapticOn()">CREATE ACCOUNT</button>
@@ -70,9 +70,46 @@ DisplayLoginForm = () => {
     `;
 }
 
+// function which is used to validate email
+function validateEmail($email) {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailReg.test( $email );
+  }
+
+// function which is used to validate password
+  function validatePassword(password) {
+    // Password should be at least 8 characters long
+    if (password.length < 8) {
+        return "Password must be at least 8 characters long.";
+    }
+
+    // Password should contain at least one lowercase letter
+    if (!/[a-z]/.test(password)) {
+        return "Password must contain at least one lowercase letter.";
+    }
+
+    // Password should contain at least one uppercase letter
+    if (!/[A-Z]/.test(password)) {
+        return "Password must contain at least one uppercase letter.";
+    }
+
+    // Password should contain at least one digit
+    if (!/\d/.test(password)) {
+        return "Password must contain at least one digit.";
+    }
+
+    // Password should contain at least one special character
+    if (!/[$@$!%*?&#^]/.test(password)) {
+        return "Password must contain at least one special character.";
+    }
+
+    // If all criteria are met, the password is valid
+    return "Password is valid.";
+}
+  
 
 // function which is used to perform validation on Register Form Data 
-function validateRegisterFormData(username,email,password,cpassword){
+function validateRegisterFormData(username,email,password,cpassword,Register_form){
     // all the parameters like username,email,... we are receiving are the html documents(tags) not data
     // now we will extract actual data from this raw elements like below :
     let usernameValue = username.value.trim();
@@ -80,11 +117,82 @@ function validateRegisterFormData(username,email,password,cpassword){
     let passwordValue = password.value.trim();
     let cpasswordValue = cpassword.value.trim();
     
+    if(usernameValue === ""){
+        displayError(username, "Username must not be empty!");
+    }else if(usernameValue.length < 3){
+        displayError(username, "Username must contains min. 3 character!");
+    }else{
+        displaySuccess(username);
+    }
+
+    if(emailValue === ""){
+        displayError(email, "Email ID must not be empty!");
+    }else if(!validateEmail(emailValue)){
+        displayError(email, "Oops! Your email id is incorrect");
+    }else{
+        displaySuccess(email);
+    }
+
+    const pRes = validatePassword(passwordValue); 
+
+    if(pRes !== "Password is valid."){
+        displayError(password, pRes);
+    }else{
+        displaySuccess(password);
+    }
+    
+    if(cpasswordValue === ""){
+        displayError(cpassword, "password must not be empty!");
+    }else if(cpasswordValue !== passwordValue){
+        displayError(cpassword, "password does not match");
+    }else{
+        displaySuccess(cpassword);
+    }
+
+    finalizeRegisterSuccess(Register_form);
+    
 }
 
 
+function displayError(inputElement, msg){
+        let parentElem = inputElement.parentElement;
+        inputElement.className = "form-control is-invalid";
+        let span = parentElem.querySelector('span');
+        span.className = "form-text text-danger";
+        span.innerText = msg;
+}
 
+function displaySuccess(inputElement){
+        let parentElem = inputElement.parentElement;
+        inputElement.className = "form-control is-valid";
+        let span = parentElem.querySelector('span');
+        span.innerText = "";
+}
 
+function showAlert(count, totalElemOfRegisterForm, msg){
+        console.log("count : "+count+" and total :"+totalElemOfRegisterForm)
+    if(count.length === totalElemOfRegisterForm){
+        swal("Good job!", msg, "success");
+
+    }
+}
+
+// function to check is the total elements of register form are valid or not
+function finalizeRegisterSuccess(Register_form){
+    
+    let totalElemOfRegisterForm = Register_form.length - 1;
+    let count = [];
+    for(let i = 0; i < Register_form.length -1; i++){
+
+            if(Register_form[i].className === "form-control is-valid"){
+                count.push(i);
+                showAlert(count,totalElemOfRegisterForm, 'Registration Completed!');
+            }else{
+                return false;
+            }
+            console.log(i);
+    }
+}
 
 
 // function to scan and get html document of Register form after setting of register form 
@@ -101,7 +209,7 @@ function scanForRegisterFormAvailability(){
         let password = document.getElementById('InputPasswordforRegister');
         let cpassword = document.getElementById('InputConfirmPasswordforRegister');
 
-        validateRegisterFormData(username,email,password,cpassword); // call the function which perform validation on register form data
+        validateRegisterFormData(username,email,password,cpassword,Register_form); // call the function which perform validation on register form data, and finally register-form tag for last operation
     });
 
 
