94c1578c90d9dda1cfc2db31df946f62c5224b33
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2023-12-29 19:35:19 +0530

    I have added an function call to call check_session() from this script to external script to quickly check for sessions after logging successfully, till now I am implemented only session operation..

--- scripts/manage_form_data.js
+++ scripts/manage_form_data.js
@@ -152,21 +152,35 @@ function validateRegisterFormData(username,email,password,cpassword,Register_for
         displaySuccess(cpassword);
     }
 
-    finalizeRegisterSuccess(Register_form);
+    finalizeRegisterSuccess(Register_form, usernameValue,emailValue,passwordValue);
     
 }
 
+function checkCredentials(emailValue,passwordValue){
+    let userdata = null;    
+    users.forEach((obj)=>{
+                if(obj.email === emailValue && obj.password === passwordValue){
+                    console.log(obj.username, obj.email, obj.password)
+                    userdata = obj;
+                }
+        });
+        if(userdata) return userdata;
+        return false;
+}
+
+
 // function which is used to validate form data
-function validateLoginFormData(email, password, Login_form){
+function validateLoginFormData(email, password, Login_form, isChecked){
         let emailValue = email.value.trim();
         let passwordValue = password.value.trim();
-
+        let i = 1;
         if(emailValue === ""){
             displayError(email, "Email must not be empty!");
         }else if(!validateEmail(emailValue)){
             displayError(email, "Oops! your email id is incorrect");
         }else{
             displaySuccess(email);
+            i++;
         }
 
         const isPasswordCorrect = validatePassword(passwordValue); 
@@ -175,9 +189,20 @@ function validateLoginFormData(email, password, Login_form){
             displayError(password, isPasswordCorrect);
         }else{
             displaySuccess(password);
+            i++;
         }
 
-        finalizeLoginSuccess(Login_form);
+        if(i === 3){
+
+            const isThere = checkCredentials(emailValue,passwordValue);
+            if(isThere){
+                finalizeLoginSuccess(Login_form,isThere,isChecked);
+            }else{
+                swal("Oops!", "Email and Password was not exists", "error");
+            }
+
+        }
+
 }
 
 
@@ -196,18 +221,48 @@ function displaySuccess(inputElement){
         span.innerText = "";
 }
 
-function showAlert(count, total, msg, type){
+function showAlert(count, total, msg, type,usernameValue,emailValue,passwordValue, isCheckedforCookie){
 
     if(type === "register"){
         console.log("Register count : "+count+" and total :"+total)
         if(count.length === total){
+            let obj = {
+                username : `${usernameValue}`,
+                email : `${emailValue}`,
+                password : `${passwordValue}`
+            };
+            users.push(obj);
             swal("Good job!", msg, "success");
             return true;
         }
     }else if(type === "login"){
         console.log("Login count :"+count+" and total : "+total);
         if(count === total){
-            swal("Wow!", msg, "success");
+            // for login usernameValue is an object which comes for cookie purpose 
+            swal(`Welcome ${usernameValue.username}!`, msg, "success");
+            if(isCheckedforCookie){ 
+                // setting up an cookie
+
+                // console.log('Remember me is Clicked!');
+                let ObjToString = encodeURIComponent(JSON.stringify(usernameValue));
+                // console.log(JSON.parse(decodeURIComponent(ObjToString)));
+
+                // Get the current date and time
+                    let now = new Date();
+
+                    // Calculate the expiration time by adding 10 minutes (600 seconds) to the current time
+                    let expireTime = now.getTime() + 600 * 1000;
+
+                    // Set the expiration time to the cookie date object
+                    now.setTime(expireTime);
+
+                    // Create the cookie string with the name, value, and expiration time
+                    document.cookie = `EcommerceUserData=${ObjToString}; expires= ${now.toUTCString()}`;
+
+
+            }
+            sessionStorage.setItem("e-comm_name", usernameValue.username);
+            check_for_session();
             return true;
         }
     }
@@ -218,7 +273,7 @@ function showAlert(count, total, msg, type){
 }
 
 // function to check is the total elements of register form are valid or not
-function finalizeRegisterSuccess(Register_form){
+function finalizeRegisterSuccess(Register_form, usernameValue,emailValue,passwordValue){
     
     let totalElemOfRegisterForm = Register_form.length - 1;
     let count = [];
@@ -226,7 +281,7 @@ function finalizeRegisterSuccess(Register_form){
 
             if(Register_form[i].className === "form-control is-valid"){
                 count.push(i);
-                showAlert(count,totalElemOfRegisterForm, 'Registration Completed!', 'register');
+                showAlert(count,totalElemOfRegisterForm, 'Registration Completed!', 'register', usernameValue,emailValue,passwordValue,null);
             }else{
                 return false;
             }
@@ -234,7 +289,7 @@ function finalizeRegisterSuccess(Register_form){
     }
 }
 
-function finalizeLoginSuccess(Login_form){
+function finalizeLoginSuccess(Login_form,userobj,isCheckedforCookie){
     
     let LoginInputTag = Login_form.querySelectorAll('.form-control');
     let totalInputElem = LoginInputTag.length-1;
@@ -243,7 +298,7 @@ function finalizeLoginSuccess(Login_form){
         if(LoginInputTag[i].className === "form-control is-valid"){
             let count = 0 + i;
             console.log(i);
-            showAlert(count, totalInputElem,"Login Successfull!",'login');
+            showAlert(count, totalInputElem,"Login Successfull!",'login',userobj,null,null,isCheckedforCookie);// sending whole object at the place of username
         }
     }
 
@@ -281,8 +336,10 @@ function scanForLoginFormAvailability(){
         
         let email = document.getElementById('InputEmail1');
         let password = document.getElementById('InputPassword1');
+        let checkbox = document.getElementById('exampleCheck1');
+        const isChecked = checkbox.checked;
         
-        validateLoginFormData(email,password, Login_form);
+        validateLoginFormData(email,password, Login_form, isChecked);
     });
 
 } // why this function, because when i am adding and changing the modal-body forms dynamically it causes eventListener failing to work
